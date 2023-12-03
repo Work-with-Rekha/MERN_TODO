62552aa480d47bf535010675ba298794fd700016
nithinRaj031197 <nithinraj.official3@gmail.com>
2023-12-03 14:29:05 +0530

    Test case

--- backend_api/server.js
+++ backend_api/server.js
@@ -5,6 +5,27 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const todos = [
+  {
+    id: "1",
+    todo: "Eat",
+    isCompleted: false,
+  },
+  {
+    id: "2",
+    todo: "Learn React.js",
+    isCompleted: false,
+  },
+  {
+    id: "3",
+    todo: "Toilet",
+    isCompleted: true,
+  },
+];
 
+app.get("/todos", (req, res) => {
+  res.status(200).json(todos);
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
